b8903a304c7722811d1f3d87bb9b72e4be343211
frascu <francescoscuccimarri@gmail.com>
2021-01-02 15:33:28 +0100

    add stateful widget

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -4,10 +4,10 @@ import 'package:english_words/english_words.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  // Stateless widgets are immutable, meaning that their properties can't change—all values are final.
+  // Stateful widgets maintain state that might change during the lifetime of the widget.
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
@@ -15,9 +15,24 @@ class MyApp extends StatelessWidget {
           title: const Text('Welcome to Flutter'),
         ),
         body: Center(
-          child: Text(wordPair.asPascalCase),
+          // child: Text(wordPair.asPascalCase),
+          child: RandomWords(),
         ),
       ),
     );
   }
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final WordPair wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+}
+
+// The RandomWords widget does little else besides creating its State class:
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
 }
